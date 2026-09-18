110c924055d018ea5a141d1b12b0d9398ff065cf
joel <foto.video.j03l@gmail.com>
2026-09-18 10:07:39 -0300

    add foot y correccion media query

--- main.js
+++ main.js
@@ -10,15 +10,18 @@ document.addEventListener("DOMContentLoaded", function () {
       { 
         title: "SISTEMA DE REGISTRO", 
         desc: "Detalles sobre el sistema de registro de datos de salud integrados.", 
-        bgImage: "https://picsum.photos/id/1018/1920/1080" 
+        bgImage: "./files/image1.jpg" 
       },
       { 
-        title: "RESISTENCIA AL AGUA", 
-        desc: "Ideal para nadar en una piscina o en el mar hasta 6 metros.", 
+        title: "REDES SOCIALES", 
+        desc: `aaa`, 
         bgImage: "https://picsum.photos/id/1025/1920/1080" 
       }
   ];
+  
 
+// <a class="text-example-enlace" target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/">Visita este sitio web</a>
+// desc: `<div class="gp-img-socialmedia"> <img class="img-test" src="./files/image-ig.png"> </div>`, 
   const section = document.getElementById("apple-section");
   const menuContainer = document.getElementById("menu-container");
   let currentIndex = 0;
@@ -73,39 +76,3 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
 });
-// document.addEventListener("DOMContentLoaded", function () {
-//   // 1. Configuramos las imágenes de fondo para cada opción
-//   // (Usa resoluciones grandes para que cubran bien toda la pantalla)
-//   const seccionData = [
-//     { bgImage: "https://picsum.photos/id/1015/1920/1080" },
-//     { bgImage: "https://picsum.photos/id/1018/1920/1080" },
-//     { bgImage: "https://picsum.photos/id/1025/1920/1080" },
-//   ];
-
-//   const section = document.getElementById("apple-section");
-//   const items = document.querySelectorAll(".menu-item");
-
-//   let currentIndex = 0;
-
-//   function updateView(index) {
-//     if (index < 0 || index >= items.length) return;
-//     currentIndex = index;
-
-//     // Actualizar clases activas
-//     items.forEach((item) => item.classList.remove("active"));
-//     items[currentIndex].classList.add("active");
-
-//     // Cambiar la imagen de fondo de toda la sección
-//     section.style.backgroundImage = `url('${seccionData[currentIndex].bgImage}')`;
-//   }
-
-//   // Cargar la imagen inicial
-//   updateView(0);
-
-//   items.forEach((item) => {
-//     item.addEventListener("click", function () {
-//       const index = parseInt(this.getAttribute("data-index"));
-//       updateView(index);
-//     });
-//   });
-// });
